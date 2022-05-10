9775f7d15bb4e2e077584158373400ef1059369d
Wagge4 <markus.wallgren4@gmail.com>
2022-05-10 21:45:36 +0000

    Add final JS code to make the calculator work the proper way

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ buttons.map( button => {
         switch(e.target.innerText){
             case 'C':
                 output.innerText = '';
+                break;
+            case '←':
+                if(output.innerText){
+                    output.innerText = output.innerText.slice(0, -1);
+                }
+                break;
+            case '=':
+                try{
+                    output.innerText = eval(output.innerText);
+                } catch {
+                    output.innerText = 'Syntax Error!';
+                }
+                break;
              default:
                 output.innerText += e.target.innerText;
         }
