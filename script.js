dd873b6d81eb0e39a338d3248995479bff516031
Wagge4 <markus.wallgren4@gmail.com>
2022-05-10 20:59:43 +0000

    Add JS code to make the buttons work, not properly yet though

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hi! You are connected successfully!")
+let output = document.getElementById('output');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                output.innerText = '';
+             default:
+                output.innerText += e.target.innerText;
+        }
+    });
+});
